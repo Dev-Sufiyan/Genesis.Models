6b73a0d0d2f1fafe58ae153b5739a6ef8a1d4b5c
Mohmad Sufiyan <sufiyan.m@surya-soft.com>
2024-09-15 22:15:08 +0530

    Added Comparision Enum

---
+++ Genesis.Models/Enums/ComparisonOperator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Genesis.Models.Enums
+{
+    public enum ComparisonOperator
+    {
+        Equal,                      // Represents equality (==)
+        NotEqual,                   // Represents inequality (!=)
+        GreaterThan,                // Represents greater than (>)
+        GreaterThanOrEqual,         // Represents greater than or equal to (>=)
+        LessThan,                   // Represents less than (<)
+        LessThanOrEqual,            // Represents less than or equal to (<=)
+        StartsWith                   // Represents starts with (String.StartsWith)
+    }
+}
