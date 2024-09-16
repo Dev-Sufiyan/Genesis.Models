266a45d2c26d6cb7e7812dc8d48255eadbaa7f0f
Mohmad Sufiyan <sufiyan.m@surya-soft.com>
2024-09-16 23:05:38 +0530

    Added Serach Param

---
+++ Genesis.Models/DTO/SearchParam.cs
@@ -0,0 +1,42 @@
+﻿using Genesis.Models.Enums;
+
+namespace Genesis.Models.DTO
+{
+    public class SearchParam
+    {
+        public string Field { get; set; }
+        public string Value { get; set; }
+        public ComparisonOperator Comparator { get; set; }
+
+        public SearchParam(string field, string value, ComparisonOperator comparator)
+        {
+            Field = field;
+            Value = value;
+            Comparator = comparator;
+        }
+        public bool Validate(out string validationMessage)
+        {
+            validationMessage = string.Empty;
+
+            if (string.IsNullOrEmpty(Field))
+            {
+                validationMessage = "Field cannot be null or empty.";
+                return false;
+            }
+
+            if (string.IsNullOrEmpty(Value))
+            {
+                validationMessage = "Value cannot be null or empty.";
+                return false;
+            }
+
+            if (!Enum.IsDefined(typeof(ComparisonOperator), Comparator))
+            {
+                validationMessage = "Invalid comparison operator.";
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
