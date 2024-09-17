838dd6d7dbbae06ac40a19d0b220b8b1809660c4
Mohmad Sufiyan <sufiyan.m@surya-soft.com>
2024-09-17 13:04:24 +0530

    Added Response Model

---
+++ Genesis.Models/Response/ErrorResponse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Genesis.Models.Response
+{
+    public class ErrorResponse
+    {
+        public string Status { get; set; }
+        public string Message { get; set; }
+        public int StatusCode { get; set; }
+
+        public ErrorResponse(int statusCode, string message, string status = "Error")
+        {
+            Status = status;
+            Message = message;
+            StatusCode = statusCode;
+        }
+    }
+}
---
+++ Genesis.Models/Response/SuccessResponse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Genesis.Models.Response
+{
+    public class SuccessResponse<T>
+    {
+        public string Status { get; set; }
+        public T Data { get; set; }
+        public int StatusCode { get; set; }
+
+        public SuccessResponse(T data, int statusCode, string status = "Success")
+        {
+            Status = status;
+            Data = data;
+            StatusCode = statusCode;
+        }
+    }
+}
