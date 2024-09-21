e90dd8b9c3189a4075f0d0f033ae3105421f81da
Mohmad Sufiyan <sufiyan.m@surya-soft.com>
2024-09-21 14:08:11 +0530

    ✏️ Attribute Rename

--- Genesis.Models/DTO/SearchParams.cs
+++ Genesis.Models/DTO/SearchParams.cs
@@ -5,7 +5,7 @@ namespace Genesis.Models.DTO
     public class SearchParams
     {
         public Pagination? Pagination { get; set; }
-        public string SortBy { get; set; } = string.Empty;
+        public string OrderBy { get; set; } = string.Empty;
         public bool IsDescending { get; set; } = false;
         public List<SearchFilters>? Filters { get; set; }
         public JoinOperator JoinOperator { get; set; } = JoinOperator.JointAnd;
